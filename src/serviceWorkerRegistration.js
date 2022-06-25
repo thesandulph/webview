b60a5dcb846b8e798503dd7249a539b3130903fe
The Sandulph <thesandulph@iSandulph-Mac.local>
2022-06-25 09:26:47 +0430

    fix

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -53,7 +53,7 @@ export function register(config) {
 }
 
 function registerValidSW(swUrl, config) {
-    console.log('=====> registerValidSW 10', swUrl, config)
+    console.log('=====> registerValidSW 11', swUrl, config)
     navigator.serviceWorker
         .register(swUrl)
         .then((registration) => {
